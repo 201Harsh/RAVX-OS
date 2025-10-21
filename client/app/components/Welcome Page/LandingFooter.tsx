37cbe951800ff47588df1e0476c47aa8207a2af2
Harsh <gamerpandeyharsh@gmail.com>
2025-10-21 17:01:55 +0530

    updated Footer

--- client/app/components/Welcome Page/LandingFooter.tsx
+++ client/app/components/Welcome Page/LandingFooter.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
 const LandingFooter = ({ itemVariants }: any) => {
+  const socials = [
+    {
+      icon: FiGithub,
+      link: "https://github.com/201Harsh",
+    },
+    {
+      icon: FiInstagram,
+      link: "https://www.instagram.com/harsh.devx/",
+    },
+    {
+      icon: FiLinkedin,
+      link: "https://www.linkedin.com/in/201harsh/",
+    },
+  ];
   return (
     <>
       <motion.footer
@@ -10,19 +26,25 @@ const LandingFooter = ({ itemVariants }: any) => {
         className="border-t border-gray-800 pt-12 pb-8"
       >
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <motion.div
-            className="flex items-center space-x-2 mb-4 md:mb-0"
-            whileHover={{ scale: 1.05 }}
-          >
-            <div className="w-2 h-2 bg-cyan-400 rounded-full animate-pulse" />
-            <span className="text-lg font-light">RAVX OS</span>
+          <motion.div whileHover={{ scale: 1.05 }}>
+            <Link
+              href="/"
+              className="flex items-center space-x-2 cursor-pointer"
+            >
+              <Image src="/img/logo.png" alt="Logo" width={32} height={32} />
+              <span className="text-xl font-light tracking-widest">
+                RAVX OS
+              </span>
+            </Link>
           </motion.div>
 
           <div className="flex space-x-6 mb-4 md:mb-0">
-            {[FiGithub, FiInstagram, FiLinkedin].map((Icon, index) => (
+            {socials.map(({ icon: Icon, link }, index) => (
               <motion.a
                 key={index}
-                href="#"
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
                 className="text-gray-400 hover:text-cyan-400 transition-colors duration-300"
                 whileHover={{ scale: 1.2, y: -2 }}
               >
