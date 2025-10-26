10e0dadab1848b86975ad465c0755fac3484b55f
Harsh <gamerpandeyharsh@gmail.com>
2025-10-26 17:56:31 +0530

    added User Route

--- server/app.js
+++ server/app.js
@@ -3,5 +3,10 @@ const ConnectToDB = require("./config/database");
 ConnectToDB();
 
 const app = express();
+const UserRouter = require("./routers/user.route");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/user", UserRouter);
 
 module.exports = app;
