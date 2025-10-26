0ea34d55d4818bc5d51a194bf572b965eb0eec2b
Harsh <gamerpandeyharsh@gmail.com>
2025-10-26 10:10:59 +0530

    Updated Router logic now from Backend Only

--- client/app/api/register/route.ts
+++
@@ -1,5 +0,0 @@
-import { Registeruser } from "@/controllers/user.controller";
-
-export async function POST(req: Request) {
-  return await Registeruser(req);
-}
--- client/app/api/verify/route.ts
+++
@@ -1,5 +0,0 @@
-import { VerifyOTP } from "@/controllers/user.controller";
-
-export async function POST(req: Request) {
-  return await VerifyOTP(req);
-}
