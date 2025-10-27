d325f8b8db35ede52d9982200c71f3597fb6028c
Harsh <gamerpandeyharsh@gmail.com>
2025-10-27 22:27:29 +0530

    Created User Model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,2 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.methods.JWT = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+};
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.static.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
