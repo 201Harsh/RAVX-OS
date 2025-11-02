2df45f646f8e520a9180e0ce7628d9609c32cef0
Harsh <gamerpandeyharsh@gmail.com>
2025-11-02 12:30:26 +0530

    added Arc Lab Model

---
+++ server/models/ArcLab.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const ArcLabSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unqique: true,
+  },
+  CreatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ArcLab = mongoose.model("ArcLab", ArcLabSchema);
+
+module.exports = ArcLab;
