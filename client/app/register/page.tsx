4af18fe4ab21c20237e9cf71350b5275413b21b5
Harsh <gamerpandeyharsh@gmail.com>
2025-11-02 12:12:25 +0530

    Updated Code and Logic and Remove Dashboard Route

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -362,7 +362,7 @@ const RegisterPage: React.FC = () => {
         });
         localStorage.setItem("token", res.data.token);
         setShowOTP(false);
-        router.push("/dashboard");
+        router.push("/ravx");
       }
     } catch (error: any) {
       console.log(error);
